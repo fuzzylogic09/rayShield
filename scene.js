2986452b28b723f1aa4a2a044f3391b2fbf982cb
fuzzylogic09 <141544831+fuzzylogic09@users.noreply.github.com>
2026-03-02 13:39:34 -0800

    Refactor materials and add ray handling methods
    Refactor materials initialization and add ray management methods.

--- scene.js
+++ scene.js
@@ -2,9 +2,7 @@ export class Scene {
 
   constructor(){
     this.objects = {};
-    this.materials = {
-      lead:{density:11.34}
-    };
+    this.materials = { lead:{density:11.34} };
     this.parameters = {
       rayNumber:1000,
       minThicknessToShow:200,
@@ -20,6 +18,16 @@ export class Scene {
     this.materials=s.materials || this.materials;
     this.parameters=s.parameters;
     this.rays=[];
+  }
+
+  addRay(ray){
+    ray.life = 1.0;   // durée de vie
+    this.rays.push(ray);
+  }
+
+  updateRays(){
+    this.rays.forEach(r=>r.life -= 0.01);
+    this.rays = this.rays.filter(r=>r.life>0);
   }
 
   export(){
